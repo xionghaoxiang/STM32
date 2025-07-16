0d840d7304df7ad72ecd08828bd51c1ec4c673c2
XHX-0315 <032310315@nuaa.edu.cn>
2025-07-16 21:42:45 +0800

    Third_day

--- PWMI模式测频率与占空比/User/main.c
+++ PWMI模式测频率与占空比/User/main.c
@@ -6,12 +6,13 @@
 int main()
 {
 	OLED_Init();
+ 
+	//为了产生一个信号，如果有信号发生器可以不用信号产生
+	PWM_Init();     
+	PWM_SetPrescaler(72-1);//修改TIM2的预分频PSR改变频率，默认在PWM_Init中ARR的值为100-1  Freq = 72M/PSR+1/ARR+1
+	PWM_SetCompare(20); //修改TIM2的与ARR比较的数值产生的信号波形的占空比
 
-	PWM_Init();
-	PWM_SetPrescaler(7200-1);
-	PWM_SetCompare(80); //修改TIM2产生的信号波形
-
-	IC_Init();
+	IC_Init(); //信号测量
 	OLED_ShowString(1,1,"Freq:00000HZ");
 	OLED_ShowString(2,1,"Duty:00%");
 		while(1)
